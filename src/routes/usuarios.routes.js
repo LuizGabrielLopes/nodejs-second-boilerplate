9dfe08b52aa0ddb3898969647928d4eb08e2e337
Luiz Gabriel <luiz.gabriellopesc@gmail.com>
2024-10-31 15:54:10 -0300

    feat: create post user method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -6,10 +6,20 @@ const usersList = new UsersRepository();
 
 usuariosRoutes.get("/", (req, res) => {
     const usuarios = usersList.getAllUsers();
-    
+
     return res.status(200).json({
         message: usuarios.length == 0 ? "Não há usuários cadastrados" : `Total de usuários: ${usuarios.length}`, usuarios
     });
 });
+
+usuariosRoutes.post("/", (req, res) => {
+    const {name, email, password} = req.body
+
+    const user = usersList.addUser(name, email, password)
+
+    return res.status(201).json({
+        message: "Usuário cadastrado com sucesso!", user
+    })
+})
 
 export default usuariosRoutes;
